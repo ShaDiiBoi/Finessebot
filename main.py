e770c1cfaee394caed3d0d28d57ebcd381f18bdc
PLofty <33667600+ShaDiiBoi@users.noreply.github.com>
2023-09-19 09:57:35 +0100

    Update main.py
    Added comments to better explain file purpose

--- main.py
+++ main.py
@@ -24,12 +24,12 @@ logging.basicConfig(level=logging.INFO)
 
 initial = ["cogs.level","cogs.suggest","cogs.gatekeeper","cogs.help","cogs.fun", "cogs.roles", "jishaku", "cogs.peep", "cogs.events", "cogs.mod", "cogs.veri", "cogs.custom", "cogs.staff","cogs.profiles"]
 
-if __name__ == "__main__":
-    for extent in initial:
+if __name__ == "__main__": #Starts the bot file.
+    for extent in initial: ## loads all file extensions/modules for bot
         try:
             bot.load_extension(extent)
         except Exception as e:
-            print(f'Failed to load extension {extent}.', file=sys.stderr)
+            print(f'Failed to load extension {extent}.', file=sys.stderr) # error catcher for extension errors on startup
             traceback.print_exc()
 
 
@@ -40,14 +40,18 @@ if __name__ == "__main__":
 #----------------------------------------------------
 #--------------------------------------------------
 @bot.command()
-@commands.is_owner()
-async def leave(ctx):
+@commands.is_owner() # only bot owner can use command
+async def leave(ctx): # leaves server command is invoked in.
     server = bot.get_guild(ctx.guild.id)
-    await server.leave()
+    try: 
+        await server.leave()
+    except Exception as e:
+        print(f"bot attempted to leave server and failed due to following error: {e}")
+    
 
 @bot.command()
-@commands.is_owner()
-async def avs(ctx, number):
+@commands.is_owner() # only bot owner can use command
+async def avs(ctx, number): # Changed bot's discord profile picture from a select folder of ordered images numbered 1 to 500.
     with open(f"/home/shadbot/pfps/peep_{number}.jpg", "rb") as fp:
         await bot.user.edit(avatar=fp.read())
     embed = discord.Embed(title="New Profile For {} Bot".format(bot.user.name))
@@ -57,13 +61,13 @@ async def avs(ctx, number):
 
 @commands.cooldown(1, 5.0, type=commands.BucketType.default)
 @bot.command()
-@commands.is_owner()
+@commands.is_owner()# only bot owner can use command
 async def game(ctx, *, game: str):
     await bot.change_presence(activity=discord.Game(name=game, type=1))
 
 @commands.cooldown(1, 5.0, type=commands.BucketType.default)
 @bot.command()
-@commands.is_owner()
+@commands.is_owner()# only bot owner can use command
 async def rename(ctx, *, name: str):
     await bot.user.edit(username=name)
 
@@ -94,9 +98,9 @@ async def rename(ctx, *, name: str):
 
 
 @bot.command()
-async def Ping(ctx):
+async def Ping(ctx): # response test
     await ctx.send('Pong!')
 
 
 
-bot.run("NTMwOTQ1ODMwMDA2MjkyNDgw.D0l0HA.paZNBNSvNc7tOs_s3NRnmMvR630")
+bot.run("NTMwOTQ1ODMwMDA2MjkyNDgw.D0l0HA.paZNBNSvNc7tOs_s3NRnmMvR630") # command to start up bot on discord's side. requires a bot token as an argument.
